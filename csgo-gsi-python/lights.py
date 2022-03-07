5875a4f222c2184a9ac2e931ea2355e7ad088560
ClemensMo <clemensmollik@gmail.com>
2022-03-07 16:55:59 +0100

    niedrige hp und bombe

--- csgo-gsi-python/lights.py
+++ csgo-gsi-python/lights.py
@@ -10,7 +10,7 @@ LED_COUNT = 300  # Number of LED pixels.
 LED_PIN = 18  # GPIO pin connected to the pixels (must support PWM!).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10  # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 128  # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
 LED_INVERT = False  # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL = 0
 LED_STRIP = ws.WS2811_STRIP_BRG
@@ -26,23 +26,33 @@ def set_flash_color(flash_value):
 
 
 def set_fire_color(fire_value):
-    c = Color(0, int(255 * (255/fire_value)), 0,)   # int(165 * (255/fire_value)), 0)
+    c = Color(int(255 * (255/fire_value)), 0, int(165 * (255/fire_value)))
     for i in range(200, strip.numPixels()):
         strip.setPixelColor(i, c)
     strip.show()
 
 
-def handle_light_events(flashed, health, bomb, fire):
+def set_base_color(r, b, g):
+    c = Color(r, b, g)
+    for i in range(0, strip.numPixels()):
+        strip.setPixelColor(i, c)
+    strip.show()
+
+
+def handle_light_events(flashed, health, bomb, fire, i):
     if flashed > 0:
         set_flash_color(flashed)
     elif fire > 0:
         set_fire_color(fire)
     elif health < 10:
-        print('Turn lights red')
+        set_base_color(10, 0, 0)
     elif bomb == 'planted':
-        print('eskalieren')
+        if i % 20 < 10:
+            set_base_color(60, 0, 30)
+        else:
+            set_base_color(60, 0, 0)
     else:
-        set_flash_color(flashed)
+        set_base_color(10, 10, 10)
 
 
 strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
--- csgo-gsi-python/main.py
+++ csgo-gsi-python/main.py
@@ -6,11 +6,12 @@ from server import GSIServer
 if __name__ == '__main__':
     server = GSIServer(("192.168.0.174", 3000), "S8RL9Z6Y22TYQK45JB4V8PHRJJMD9DS9")
     server.start_server()
-
+    i = 0
     while True:
+        i += 1
         print(server.gamestate.map.name)
         print(server.get_info("player", "state", "flashed"))
         handle_light_events(server.get_info("player", "state", "flashed"),
                             server.get_info("player", "state", "flashed"),
                             server.get_info("player", "state", "health"),
-                            server.get_info("player", "state", "burning"))
+                            server.get_info("player", "state", "burning"), i)
